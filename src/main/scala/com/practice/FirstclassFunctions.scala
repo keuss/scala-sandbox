2834245566357df651428d4b6d1936e35a0d457e
guillaume gallois <guillaume.gallois@gmail.com>
2015-11-13 17:30:39 +0100

    add FirstclassFunctions

--- src/main/scala/com/practice/FirstclassFunctions.scala
+++ src/main/scala/com/practice/FirstclassFunctions.scala
@@ -2,4 +2,28 @@ package com.practice
 
 object FirstclassFunctions extends App {
 
+  def square(a: Int) = a * a
+
+  def squareWithBlock(a: Int) = {
+    a * a
+  }
+
+  val squareVal = (a: Int) => a * a
+
+  def addOne(f: Int => Int, arg: Int) = f(arg) + 1
+
+  println("square(2):" + square(2))
+  println("squareWithBlock(2):" + squareWithBlock(2))
+  println("squareVal(2):" + squareVal(2))
+  println("addOne(squareVal,2):" + addOne(squareVal, 2))
+
+  println("-----FONCTION AS VARIABLE-----")
+  def addTag(tag:String): (String => String) = {
+    (text:String) => "<" + tag + ">" + text + "</" + tag + ">"
+  }
+
+  val addSpan = addTag("span")
+  println(addSpan("hello !"))
+  println(addSpan("bye !"))
+
 }
---
+++ src/main/scala/com/practice/Tuples.scala
@@ -0,0 +1,6 @@
+package com.practice
+
+object Tuples extends App {
+
+  // TODO
+}

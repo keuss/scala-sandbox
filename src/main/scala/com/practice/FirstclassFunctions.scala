5d2c2fee5e1951fc16ac310f3f00d5962a873bf6
guillaume gallois <guillaume.gallois@gmail.com>
2015-11-14 16:21:32 +0100

    update FirstclassFunctions and tuples

--- src/main/scala/com/practice/FirstclassFunctions.scala
+++ src/main/scala/com/practice/FirstclassFunctions.scala
@@ -2,6 +2,9 @@ package com.practice
 
 object FirstclassFunctions extends App {
 
+  // Function Type Definition
+  // And Function as variable ---
+
   def square(a: Int) = a * a
 
   def squareWithBlock(a: Int) = {
@@ -9,6 +12,9 @@ object FirstclassFunctions extends App {
   }
 
   val squareVal = (a: Int) => a * a
+  val doubleVal: (Int => Int) = (a: Int) => a * 2
+  //val doubleVal: (Int) => Int = (a) => a * 2
+  //val doubleVal: Int => Int = a => a * 2
 
   def addOne(f: Int => Int, arg: Int) = f(arg) + 1
 
@@ -16,8 +22,9 @@ object FirstclassFunctions extends App {
   println("squareWithBlock(2):" + squareWithBlock(2))
   println("squareVal(2):" + squareVal(2))
   println("addOne(squareVal,2):" + addOne(squareVal, 2))
+  println("doubleVal(3):" + doubleVal(3))
+  println("addOne(doubleVal,3):" + addOne(doubleVal, 3))
 
-  println("-----FONCTION AS VARIABLE-----")
   def addTag(tag:String): (String => String) = {
     (text:String) => "<" + tag + ">" + text + "</" + tag + ">"
   }
@@ -25,5 +32,50 @@ object FirstclassFunctions extends App {
   val addSpan = addTag("span")
   println(addSpan("hello !"))
   println(addSpan("bye !"))
+  // ----------------------------
+
+  // Call by Name ----------------
+  // A way to pass a literal function as a code block
+
+  def execute(sql: String): Boolean = {
+    println(s"$sql execution ...")
+    true
+  }
+
+  def transaction(code: => Boolean) = {
+    //connect to DB, grab a connection, start transaction
+    //...
+    //execute the code
+    code
+    //if things went ok commit, if not rollback
+  }
+
+  transaction {
+    execute("INSERT INTO SOME_TABLE...")
+  }
+  // ----------------------------
+
+  // Higher-order : A function that can
+  // Be passed a function / Return a function
+  // Closure !
+  case class Employee(id: Int, name: String)
+  def reallySlowTaxCalculator(emp: Employee): () => Double = {
+    () => {
+      println(s"reallySlowTaxCalculator called fom emp=${emp.name}")
+      Thread.sleep(5000);
+      102.21
+    }
+  }
+
+  def deferTaxCalculate(emp: Employee): () => Double = {
+    reallySlowTaxCalculator(emp)
+  }
+
+  // We want do the slow reallySlowTaxCalculator now ...
+  val empTax = deferTaxCalculate(Employee(1, "Leyla"))
+  // do other stuff ...
+  // Now calculate tax for Leyla (now we have time ...)
+  println(empTax());
+  // ----------------------------
 
 }
--- src/main/scala/com/practice/Tuples.scala
+++ src/main/scala/com/practice/Tuples.scala
@@ -2,5 +2,22 @@ package com.practice
 
 object Tuples extends App {
 
-  // TODO
+  sealed trait FitnessDevice
+
+  case class Barbell(load: Int, length: Int) extends FitnessDevice {
+    def weigh: Barbell = Barbell(load + 10, length + 20)
+    def lighten: Barbell = Barbell(load - 2, length - 20)
+  }
+
+  case class Mat(width: Int, length: Int) extends FitnessDevice {
+    // utilisation Option
+    def smaller: Option[Mat] =
+      if (width <= 15 || length <= 100) None
+      else Some(Mat(width - 10, length - 20))
+  }
+
+  val fitnessEquipment:(FitnessDevice, FitnessDevice, FitnessDevice) = (Barbell(30, 50), Barbell(20, 50), Mat(30, 100))
+  println(fitnessEquipment)
+  println(fitnessEquipment._2)
+
 }

1fa6f292fa5b02a0a5cda889c418a22077d3f0e1
guillaume gallois <guillaume.gallois@gmail.com>
2015-11-21 18:27:29 +0100

    and Promises and Futures exemples

--- src/main/scala/com/practice/FuturesAndPromises.scala
+++ src/main/scala/com/practice/FuturesAndPromises.scala
@@ -2,21 +2,111 @@ package com.practice
 
 import concurrent.Future
 import concurrent.ExecutionContext.Implicits.global
-import scala.util.{Failure, Success}
-import scala.util.Random
+import scala.util.{Try, Failure, Success, Random}
+import scala.concurrent.duration._
 
 object FuturesAndPromises extends App {
 
-  // First exemple "Hello World"
+  // Futures & Promises
+  // First exemple "Hello World" -
   val f: Future[String] = Future {
     "Hello world!!"
   }
 
-  println("Exemple Future ...")
-  Thread.sleep(2000)
+  println("Exemple Promises ...")
+
   f.onComplete {
     case Success(value) => println(value)
     case Failure(error) => println(error)
   }
   println("Exemple Future end")
+  Thread.sleep(2000)
+
+  // exemple from http://danielwestheide.com/blog/2013/01/09/the-neophytes-guide-to-scala-part-8-welcome-to-the-future.html
+
+  // Sequential code -------------
+
+  // Some type aliases, just for getting more meaningful method signatures:
+  type CoffeeBeans = String
+  type GroundCoffee = String
+  case class Water(temperature: Int)
+  type Milk = String
+  type FrothedMilk = String
+  type Espresso = String
+  type Cappuccino = String
+  // dummy implementations of the individual steps:
+  /*def grind(beans: CoffeeBeans): GroundCoffee = s"ground coffee of $beans"
+  def heatWater(water: Water): Water = water.copy(temperature = 85)
+  def frothMilk(milk: Milk): FrothedMilk = s"frothed $milk"
+  def brew(coffee: GroundCoffee, heatedWater: Water): Espresso = "espresso"*/
+  def combine(espresso: Espresso, frothedMilk: FrothedMilk): Cappuccino = "cappuccino"
+  // some exceptions for things that might go wrong in the individual steps
+  // (we'll need some of them later, use the others when experimenting
+  // with the code):
+  case class GrindingException(msg: String) extends Exception(msg)
+  case class FrothingException(msg: String) extends Exception(msg)
+  case class WaterBoilingException(msg: String) extends Exception(msg)
+  case class BrewingException(msg: String) extends Exception(msg)
+  // going through these steps sequentially:
+  /*def prepareCappuccino(): Try[Cappuccino] = for {
+    ground <- Try(grind("arabica beans"))
+    water <- Try(heatWater(Water(25)))
+    espresso <- Try(brew(ground, water))
+    foam <- Try(frothMilk("milk"))
+  } yield combine(espresso, foam)*/
+
+  /*prepareCappuccino() match {
+    case Success(c) => println(s"$c is ready !")
+    case Failure(ex) => println(s"Problem prepareCappuccino: ${ex.getMessage}")
+  }*/
+
+  // Working with Futures
+  def grind(beans: CoffeeBeans): Future[GroundCoffee] = Future {
+    println("start grinding...")
+    Thread.sleep(Random.nextInt(2000))
+    if (beans == "baked beans") throw GrindingException("are you joking?")
+    println("finished grinding...")
+    s"ground coffee of $beans"
+  }
+
+  def heatWater(water: Water): Future[Water] = Future {
+    println("heating the water now")
+    Thread.sleep(Random.nextInt(2000))
+    println("hot, it's hot!")
+    water.copy(temperature = 85)
+  }
+
+  def frothMilk(milk: Milk): Future[FrothedMilk] = Future {
+    println("milk frothing system engaged!")
+    Thread.sleep(Random.nextInt(2000))
+    println("shutting down milk frothing system")
+    s"frothed $milk"
+  }
+
+  def brew(coffee: GroundCoffee, heatedWater: Water): Future[Espresso] = Future {
+    println("happy brewing :)")
+    Thread.sleep(Random.nextInt(2000))
+    println("it's brewed!")
+    "espresso"
+  }
+
+  def prepareCappuccino(): Future[Cappuccino] = {
+    val groundCoffee = grind("arabica beans")
+    val heatedWater = heatWater(Water(20))
+    val frothedMilk = frothMilk("milk")
+    for {
+      ground <- groundCoffee
+      water <- heatedWater
+      foam <- frothedMilk
+      espresso <- brew(ground, water)
+    } yield combine(espresso, foam)
+  }
+
+  prepareCappuccino().onComplete {
+    case Success(value) => println(value)
+    case Failure(error) => println(error)
+  }
+
+  Thread.sleep(10000)
+
 }

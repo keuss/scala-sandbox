f2103790875f039fb8735091362b37058d123634
guillaume gallois <guillaume.gallois@gmail.com>
2016-05-13 13:26:57 +0200

    update json play2 basics tests

--- src/main/scala/com/practice/play2/TestJSON.scala
+++ src/main/scala/com/practice/play2/TestJSON.scala
@@ -9,6 +9,7 @@ object TestJSON extends App {
 
   println("\nTestJSON -----------------------------")
   // examples from https://www.playframework.com/documentation/2.4.x/ScalaJson
+  // and https://www.playframework.com/documentation/2.4.x/ScalaJsonCombinators
 
   val json: JsValue = Json.parse("""
   {
@@ -30,6 +31,18 @@ object TestJSON extends App {
   """)
   println(json)
 
+  // {"factures":{"numBordereau":26030491180,"montant":1000.0,"nbDocs":1},"avoirs":{"numBordereau":26030491181,"montant":100.0,"nbDocs":1}}
+  val json2: JsValue = Json.parse("""
+  {
+    "factures" : {
+    "numBordereau":26030491180,
+    "montant":1000.0,
+    "nbDocs":1
+    }
+  }
+  """)
+  println(json2)
+
   // Traversing a JsValue structure
   println("\nTraversing a JsValue structure --------")
   val lat = (json \ "location" \ "lat").get
@@ -41,6 +54,29 @@ object TestJSON extends App {
   val bigwig = (json \ "residents")(1).get
   println(s"${bigwig}")
 
-  //TODO other real tests
+  val factures = (json2 \ "factures" \ "numBordereau")
+  println(s"${factures}")
+
+  val mayBefacture = (json2 \ "factures")
+  val numBordereauOK = mayBefacture match {
+    case js: JsUndefined => {
+      println("KO : " + js)
+      "0"
+    }
+    case js: JsDefined => {
+      println("OK : " + js)
+      (js \ "numBordereau").get
+    }
+  }
+  println(s"${numBordereauOK}")
+
+  val numBordereau = json2 \\ "numBordereau"
+  println(s"${numBordereau}")
+
+  // Converting from a JsValue
+  println("\nConverting from a JsValue ------------")
+  val name = (json \ "name").as[String]
+  println(s"${name}")
+
 
 }
